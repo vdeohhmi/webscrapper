63c5ea3e09bfd51b46f5e4797cd7415e83dd0364
vdeohhmi <vdeohhmi@gmail.com>
2025-05-20 18:44:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from scraper import AdvancedScraper
 
 if __name__ == "__main__":
-    url = input("Enter the URL to scrape: ").strip()
+    url = os.getenv("SCRAPER_URL")
+    if not url:
+        raise ValueError("SCRAPER_URL environment variable not set")
     scraper = AdvancedScraper(start_url=url)
     scraper.run()
